27d699bbb38528964de187cb61a7a5770d5db1b8
evgeny.yulov <Evgeny.Yulov@x5.ru>
2022-01-30 22:06:03 +0300

    commit for test chromatic

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -20,22 +20,22 @@ export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   primary: true,
-  label: 'Button',
+  label: 'Primary',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
+  label: 'Secondary',
 };
 
 export const Large = Template.bind({});
 Large.args = {
   size: 'large',
-  label: 'Button',
+  label: 'Large',
 };
 
 export const Small = Template.bind({});
 Small.args = {
   size: 'small',
-  label: 'Button',
+  label: 'Small',
 };
