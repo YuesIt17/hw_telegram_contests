c73891b544b27ffe3be5c8b8c34b5b3022949d15
evgeny.yulov <Evgeny.Yulov@x5.ru>
2022-05-10 21:06:10 +0300

    test deploy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  assetPrefix: isProd ? '/hw_telegram_contests/' : '',
+  assetPrefix: isProd ? '/hw_telegram_contests' : '',
   env: {
     REACT_URL_API: 'http://localhost:5000/'
   },
