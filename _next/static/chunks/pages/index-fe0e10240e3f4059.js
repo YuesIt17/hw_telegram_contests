2808e3eed6380d823404c0849ff504b7687142d3
YuesIt17 <YuesIt17@users.noreply.github.com>
2022-05-14 15:54:46 +0000

    deploy: 9bbdd3a32b1aa00efdcf1570ed9964bcb109d9cb

---
+++ _next/static/chunks/pages/index-fe0e10240e3f4059.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{2779:function(n,u,_){"use strict";_.r(u);var t=_(8880);u.default=t.ZP},8312:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return _(2779)}])}},function(n){n.O(0,[774,888,179],(function(){return u=8312,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
