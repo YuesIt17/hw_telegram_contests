45543bf140967f91685ec092d435b4ba5600eeb6
evgeny.yulov <Evgeny.Yulov@x5.ru>
2022-05-16 21:09:07 +0300

    fix by review for to catch error

--- src/api/index.ts
+++ src/api/index.ts
@@ -7,7 +7,15 @@ export class Fetcher {
   static baseApiUrl = `${window.location.protocol}//${window.location.hostname}:${process.env.REACT_PORT_API}`;
 
   public static async request({url, init}: TRequest) {
-    const response = await fetch(`${this.baseApiUrl}/${url}`, init);
-    return response.json();
+    try {
+      const response = await fetch(`${this.baseApiUrl}/${url}`, init);
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Network response error.');
+    } catch (e) {
+      const message = e instanceof Error ? e.message : '500 server error';
+      throw new Error(`${message}`);
+    }
   }
 }
